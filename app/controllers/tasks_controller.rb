cbcf0f47d42e071a311fd906b88b1314df0f8557
yuusuke.nishimura <west246x@gmail.com>
2018-01-22 03:19:33 +0000

    change destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,6 +40,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
   end
   
    private
