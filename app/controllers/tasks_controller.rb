fbd8e5dad70754bf7a9ef49ac903a3478cbfb186
yuusuke.nishimura <west246x@gmail.com>
2018-01-22 03:17:49 +0000

    change update

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,15 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
