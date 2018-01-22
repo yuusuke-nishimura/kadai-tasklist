37406ec9696d0fd99d584344d13054c350b07361
yuusuke.nishimura <west246x@gmail.com>
2018-01-22 13:34:54 +0000

    final cheack

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(message_params)
+     @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
