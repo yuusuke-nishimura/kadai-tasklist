01d196bfc2fb86f6574011dd7485dc28069e958b
yuusuke.nishimura <west246x@gmail.com>
2018-01-25 09:26:24 +0000

    kadai kansei

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,20 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in, only: [:index, :show]
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
+    @user =  current_user
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.new
   end
 
   def create
-     @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
@@ -24,11 +26,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -40,7 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+    @task = current_user.tasks.find(params[:id])
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :status, presence: true, length: { maximum: 10 }
+  validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 255 }
 end
